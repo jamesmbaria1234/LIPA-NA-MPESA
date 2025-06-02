83cddf11ce81fb096d824cda38f5b37338ca0295
jamesmbaria1234 <jamesmbaria20@gmail.com>
2025-06-02 12:45:56 +0300

    add server url

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import acessToken from './Routes/Token.route.js'
 config();
 
 const app = express()
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: ["http://localhost:5173","https://lipa-na-mpesa.vercel.app/" ]}));
 
 app.use (express.json())
 app.use(express.urlencoded({ extended: true }));
